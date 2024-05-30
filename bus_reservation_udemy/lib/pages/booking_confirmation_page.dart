adb5cd0422a9085662503b653e63f6a51266462b
Shahin Safaroff <tungsten.rust@gmail.com>
2024-05-31 00:57:04 +0400

    Booking Confirmation Page is done

--- bus_reservation_udemy/lib/pages/booking_confirmation_page.dart
+++ bus_reservation_udemy/lib/pages/booking_confirmation_page.dart
@@ -1,3 +1,6 @@
+import 'package:bus_reservation_udemy/models/bus_schedule.dart';
+import 'package:bus_reservation_udemy/utils/constants.dart';
+import 'package:bus_reservation_udemy/utils/helper_functions.dart';
 import 'package:flutter/material.dart';
 
 class BookingConfirmationPage extends StatefulWidget {
@@ -8,8 +11,169 @@ class BookingConfirmationPage extends StatefulWidget {
 }
 
 class _BookingConfirmationPageState extends State<BookingConfirmationPage> {
+  late BusSchedule schedule;
+  late String departureDate;
+  late int totalSeatsBooked;
+  late String seatNumbers;
+  bool isFirst = true;
+  final _formKey = GlobalKey<FormState>();
+  final nameController = TextEditingController();
+  final mobileController = TextEditingController();
+  final emailController = TextEditingController();
+
+  @override
+  void initState() {
+    nameController.text = 'Mr. ABC';
+    mobileController.text = '0123452433';
+    emailController.text = 'abc@gmail.com';
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    if(isFirst){
+      final argList = ModalRoute.of(context)!.settings.arguments as List;
+      departureDate = argList[0];
+      schedule = argList[1];
+      seatNumbers = argList[2];
+      totalSeatsBooked = argList[3];
+      isFirst = false;
+    }
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Confirm Booking'),),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: const EdgeInsets.all(8.0),
+          children: [
+            const Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: const Text('Please provide your information',
+                style: TextStyle(fontSize: 16),),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  hintText: 'Customer Name',
+                  filled: true,
+                  prefixIcon: const Icon(Icons.person),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return emptyFieldErrMessage;
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  setState(() {
+
+                  });
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: mobileController,
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  hintText: 'Mobile Number',
+                  filled: true,
+                  prefixIcon: const Icon(Icons.call),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return emptyFieldErrMessage;
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  setState(() {
+
+                  });
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Email Address',
+                  filled: true,
+                  prefixIcon: const Icon(Icons.email),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return emptyFieldErrMessage;
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  setState(() {
+
+                  });
+                },
+              ),
+            ),
+            const Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: const Text(
+                'Booking Summery', style: TextStyle(fontSize: 16),),
+            ),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Customer Name: ${nameController.text}'),
+                    Text('Mobile Number: ${mobileController.text}'),
+                    Text('Email Address: ${emailController.text}'),
+                    Text('Route: ${schedule.busRoute.routeName}'),
+                    Text('Departure Date: $departureDate'),
+                    Text('Departure Time: ${schedule.departureTime}'),
+                    Text('Ticket Price: $currency${schedule.ticketPrice}'),
+                    Text('Total Seat(s): $totalSeatsBooked'),
+                    Text('Seat Number(s): $seatNumbers'),
+                    Text('Discount: ${schedule.discount}%'),
+                    Text('Processing Fee: ${schedule.processingFee}%'),
+                    Text('Grand Total: $currency${getGrandTotal(
+                        schedule.discount, totalSeatsBooked,
+                        schedule.ticketPrice, schedule.processingFee)}%',
+                      style: TextStyle(fontSize: 18,),)
+                  ],
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: _confirmBooking,
+              child: const Text('CONFIRM BOOKING'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _confirmBooking(){
+    if(_formKey.currentState!.validate()){
+
+    }
+  }
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    mobileController.dispose();
+    super.dispose();
   }
 }
--- bus_reservation_udemy/lib/utils/helper_functions.dart
+++ bus_reservation_udemy/lib/utils/helper_functions.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -9,3 +8,9 @@ String getFormattedDate(DateTime dt, {String pattern = 'dd/MM/yyyy'}){
 void showMsg(BuildContext context, String msg) =>
     ScaffoldMessenger.of(context)
         .showSnackBar(SnackBar(content: Text(msg)));
+
+int getGrandTotal(int discount, int totalSeatBooked, int price, int fee){
+  final subTotal = totalSeatBooked * price;
+  final priceAfterDiscount = subTotal - ((subTotal * discount)/100);
+  return (priceAfterDiscount + fee).toInt();
+}
